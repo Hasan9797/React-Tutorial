0224e4e196e941b4c3f1398a9aa8367624150ef5
hasan <hasanjonsadullayev97@gmail.com>
2022-05-14 08:52:34 -0700

    Sof React JS dan JSX ga o'girilgan kichik loyiha

--- amalyot/src/index.js
+++ amalyot/src/index.js
@@ -3,53 +3,68 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 // import App from './App';
 
-
-// #### Sof React /loyiha Code ####
-
-// const Book = (props) => {
-//   return React.createElement('div', {}, [
-//       React.createElement('h2', {}, props.name),
-//       React.createElement('p', {}, props.year),
-//       React.createElement('p', {}, props.price)
-//   ])
-// }
+// const News = (props) => {
+//   return React.createElement("div", {}, [
+//     React.createElement("h2", {}, props.name),
+//     React.createElement("div", { className: "box" }, [
+//       React.createElement("p", {}, props.extra),
+//       React.createElement("p", {}, props.text),
+//     ]),
+//     React.createElement("div", { className: "square" }, [
+//       React.createElement("h1", {}, props.header),
+//     ]),
+//   ]);
+// };
 
 // const App = () => {
-//   return React.createElement('div', {}, [
-//       React.createElement('h1', {id: 'title', className: 'paragraph'}, "Hello world"),
-//       React.createElement(Book, {name: 'JS for beginners', year: 2019, price: 50+'$'}),
-//       React.createElement(Book, {name: 'React for beginners', year: 2020, price: 60+'$'}),
-//       React.createElement(Book, {name: 'Vue for beginners', year: 2021, price: 70+'$'}),
-//   ])
-// }
+//   return React.createElement("div", {}, [
+//     React.createElement("div", {}, [
+//       React.createElement(News, {
+//         name: "BBC News",
+//         extra: 2020,
+//         text: "Hasan Sa'dullaev",
+//         header: "BigJob",
+//       }),
+//     ]),
+//   ]);
+// };
 // ReactDOM.render(React.createElement(App), document.getElementById("root"))
 
-
 // ### JSX da yozilishi va avzalligi /loyiha Code###
-const News = (props) => {
-  return React.createElement("div", {}, [
-    React.createElement("h2", {}, props.name),
-    React.createElement("div", { className: "box" }, [
-      React.createElement("p", {}, props.extra),
-      React.createElement("p", {}, props.text),
-    ]),
-    React.createElement("div", { className: "square" }, [
-      React.createElement("h1", {}, props.header),
-    ]),
-  ]);
+const News = ({name, extra, text, header}) => {
+  return (
+    <div>
+
+      <h2>{name}</h2>
+      <div className="box">
+        <p>{extra}</p>
+        <p>{text}</p>
+      </div>
+
+      <div className="square">
+        <h1>{header}</h1>
+      </div>
+
+    </div>
+  );
 };
 
 const App = () => {
-  return React.createElement("div", {}, [
-    React.createElement("div", {}, [
-      React.createElement(News, {
-        name: "BBC News",
-        extra: 2020,
-        text: "Hasan Sa'dullaev",
-        header: "BigJob",
-      }),
-    ]),
-  ]);
+  return <div>
+    <News name="BBC News" extra="2020" text="Hasan Sa'dullaev" header="BigJob"/>
+  </div>
+  
+  //Sof React /loyiha Code
+  // return React.createElement("div", {}, [
+  //   React.createElement("div", {}, [
+  //     React.createElement(News, {
+  //       name: "BBC News",
+  //       extra: 2020,
+  //       text: "Hasan Sa'dullaev",
+  //       header: "BigJob",
+  //     }),
+  //   ]),
+  // ]);
 };
 
 const root = ReactDOM.createRoot(document.getElementById("root"));

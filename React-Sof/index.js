9dfdb968570528958a83c4ab3df0ccc23effe50d
hasan <hasanjonsadullayev97@gmail.com>
2022-05-14 04:37:56 -0700

    Sof React va JSX farqi

---
+++ React-Sof/index.js
@@ -0,0 +1,19 @@
+
+const Book = (props) => {
+    return React.createElement('div', {}, [
+        React.createElement('h2', {}, props.name),
+        React.createElement('p', {}, props.year),
+        React.createElement('p', {}, props.price)
+    ])
+}
+
+const App = () => {
+    return React.createElement('div', {}, [
+        React.createElement('h1', {id: 'title', className: 'paragraph'}, "Hello world"),
+        React.createElement(Book, {name: 'JS for beginners', year: 2019, price: 50+'$'}),
+        React.createElement(Book, {name: 'React for beginners', year: 2020, price: 60+'$'}),
+        React.createElement(Book, {name: 'Vue for beginners', year: 2021, price: 70+'$'}),
+    ])
+}
+
+ReactDOM.render(React.createElement(App), document.getElementById("root"))
